10cfe290c45a5d1b66b60ad658e15f69b0e143d8
Tyuiu.BaldinAA@outlook.com <artyombaldin94438@gmail.com>
2024-09-28 23:14:37 +0500

    Add project files.

---
+++ Tyuiu.BaldinAA.Sprint1.Task0.V6/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.BaldinAA.Sprint1.Task0.V6.Lib;
+namespace Tyuiu.BaldinAA.Sprint1.Task0.V6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Балдин А. А. | ИБКСб-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #6                                                              *");
+            Console.WriteLine("* Выполнил: Балдин А. А. | ИБКСб-24-1                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет выражение 3*(4+1) и               *");
+            Console.WriteLine("* печатает результат на экране                                            *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate());
+        }
+    }
+}
