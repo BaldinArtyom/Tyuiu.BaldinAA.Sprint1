901da46e145be0f68a2429aa6940b00ed79fc525
Tyuiu.BaldinAA@outlook.com <artyombaldin94438@gmail.com>
2024-09-29 21:45:25 +0500

    Task2

---
+++ Tyuiu.BaldinAA.Sprint1.Task2.V23.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+using Tyuiu.BaldinAA.Sprint1.Task2.V23.Lib;
+namespace Tyuiu.BaldinAA.Sprint1.Task2.V23.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 15;
+            var res = ds.ConvertMinutesToSeconds(x);
+            Assert.AreEqual(900, res);
+
+
+        }
+    }
+}
---
+++ Tyuiu.BaldinAA.Sprint1.Task2.V23/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.BaldinAA.Sprint1.Task2.V23.Lib;
+namespace Tyuiu.BaldinAA.Sprint1.Task2.V23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Балдин А. А. | ИБКСб-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #23                                                              *");
+            Console.WriteLine("* Выполнил: Балдин А. А. | ИБКСб-24-1                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x;
+
+            Console.WriteLine("");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Время в минутах = " + ds.ConvertMinutesToSeconds(x));
+
+         
+        }
+    }
+}
