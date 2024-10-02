7b01261839db1fe0fb4ad0190f72ac4be3d28a3c
Tyuiu.BaldinAA@outlook.com <artyombaldin94438@gmail.com>
2024-10-02 23:57:18 +0500

    Task4

---
+++ Tyuiu.BaldinAA.Sprint1.Task4.V12/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.BaldinAA.Sprint1.Task4.V12.Lib;
+namespace Tyuiu.BaldinAA.Sprint1.Task4.V12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Балдин А. А. | ИБКСб-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #12                                                             *");
+            Console.WriteLine("* Выполнил: Балдин А. А. | ИБКСб-24-1                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x, y;
+
+            Console.WriteLine("Введите значение x: ");
+
+            Console.WriteLine("Введите значение ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("sinpx/x+(|y|)^1/2 = " + ds.Calculate(x,y));
+        }
+    }
+}
